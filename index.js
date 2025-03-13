105b91a555b98dbb9ac31ed0f51819db09c8f58e
André Felipe Pinto <andrbter@gmail.com>
2025-03-13 12:28:48 -0300

    a cada 3 horas agora

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function autoRebootChannels() {
   });
 }
 
-cron.schedule('0 * * * *', autoRebootChannels);
+cron.schedule('0 */3 * * *', autoRebootChannels);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Servidor rodando na porta " + (process.env.PORT || 5000));
